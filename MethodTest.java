bcf3d95f6407b12d16373fc70af9518c2e4f44f5
MrBrown969 <115537578+MrBrown969@users.noreply.github.com>
2022-10-13 13:22:44 +0300

    Add files via upload
    Запуск через CalcMainBody

---
+++ MethodTest.java
@@ -0,0 +1,24 @@
+
+package Calculator;
+
+
+
+public class MethodTest {
+    
+ //  public static String calc(){
+    
+ //  String split = new String(input);
+    
+  //  String [] plus = split.split(" [-] ");
+   
+  // int number1 = Integer.parseInt(plus[0]);
+   
+  // int number2 = Integer.parseInt(plus[1]);
+   
+    // if (number1 > 10 || number1<1||number2>10||number2<1){
+       
+   //  System.out.println("Ой, что-то пошло не так.Обрати внимание, что ты можешь вводить только числа с 1 до 10 включительно");
+    
+//}
+   
+   }
